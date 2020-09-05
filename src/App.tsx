f90148c0d89b1eb2dae88452a4688b6de1cfec9f
aniketdassrit1 <aniketdassrit1@gmail.com>
2020-09-05 20:15:21 +0530

    [feature] added basic layout of the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import SideNav from "./components/sidenav/Sidenav";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <div className="container-fluid h-100">
+        <div className="row h-100">
+          <div className="col-2">
+            <SideNav />
+          </div>
+          <div className="col-10">
+            <h1>Main content</h1>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/sidenav/Sidenav.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SideNav = () => {
+  return (
+    <nav className="h-100">
+      <div className="d-flex flex-column h-100">
+        <div className="profile">Profile</div>
+        <div className="nav-menu">List of options</div>
+      </div>
+    </nav>
+  );
+};
+
+export default SideNav;
